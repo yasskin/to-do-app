da42d633098424a735030bc7497450bf4ca4a0f3
Stephanie Gurung <stephr3@gmail.com>
2016-08-07 11:52:15 -0700

    annotate component

---
+++ app/app.component.ts
@@ -0,0 +1,6 @@
+@Component({
+  selector: 'my-app', //names HTML tag
+  template: `
+    <h1>Skeleton Angular2 App!</h1>
+  ` //Gives html to be displayed in this tag. `` allows multiple lines (feature of ES6)
+})

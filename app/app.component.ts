4169599993e2fde99b1876c52edc82d73fe61ea5
Stephanie Gurung <stephr3@gmail.com>
2016-08-07 12:38:30 -0700

    add container div to my-app component

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from 'angular2/core';
 @Component({
   selector: 'my-app', //names HTML tag
   template: `
-    <h1>Skeleton Angular2 App!</h1>
+    <div class="container">
+      <h1>Skeleton Angular2 App!</h1>
+    </div>
   ` //Gives html to be displayed in this tag. `` allows multiple lines (feature of ES6)
 })
 export class AppComponent {

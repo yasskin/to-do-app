1651250b1139a45afa993fa4b50a09bd31150e59
Stephanie Gurung <stephr3@gmail.com>
2016-08-07 11:59:32 -0700

    initialize main.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,12 @@
+import { Component } from 'angular2/core';
+
 @Component({
   selector: 'my-app', //names HTML tag
   template: `
     <h1>Skeleton Angular2 App!</h1>
   ` //Gives html to be displayed in this tag. `` allows multiple lines (feature of ES6)
 })
+
+export class AppComponent {
+
+}
---
+++ app/main.ts
@@ -0,0 +1,4 @@
+import {bootstrap} from 'angular2/platform/browser'
+import {appComponent} from './app.component'
+
+bootstrap(AppComponent); //defines entry point of app
